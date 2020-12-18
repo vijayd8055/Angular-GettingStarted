4799bb141baa17fb93d0ac932d9c0ec6c6abd868
Vijay Kumar <vijaykr391807@gmail.com>
2020-12-18 16:51:40 +0530

    created product-list component

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ProductListComponent } from './products/product-list.component';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule
